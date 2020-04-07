5e1728aa17637455d837a41590615ca8f1a209c6
Morten Piibeleht <morten.piibeleht@gmail.com>
2020-04-07 21:04:23 +1200

    KaTeX example

--- docs/make.jl
+++ docs/make.jl
@@ -1,5 +1,7 @@
 using Documenter, Example
 
-makedocs(modules = [Example], sitename = "Example.jl", format=Documenter.HTML(mathengine=MathJax()))
+makedocs(modules = [Example], sitename = "Example.jl", format=Documenter.HTML(
+	mathengine=KaTeX(Dict(:globalGroup => true, :macros => Dict("\\x" => "\\boldsymbol{x}")))
+))
 
 deploydocs(repo = "github.com/JuliaLang/Example.jl.git")
--- docs/src/index.md
+++ docs/src/index.md
@@ -1,7 +1,9 @@
 # Example
 
+
+where ``N(h) = \left\{(i,j) \mid ||\x_i - \x_j|| = h\right\}`` ...
+
 ```math
-\newcommand{\x}{\boldsymbol{x}}
 \hat{\gamma}(h) = \frac{1}{2|N(h)|} \sum_{(i,j) \in N(h)} (z_i - z_j)^2
 ```
 
